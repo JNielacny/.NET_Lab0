6bdcf7272cb589d99e6df0dfcc2f5e377fdbba82
JNielacny <103131107+JNielacny@users.noreply.github.com>
2024-03-03 20:48:15 +0100

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class FizzBuzz
+    {
+        int max = 0;
+        public FizzBuzz(int upmax)
+        {
+            this.max = upmax;
+        }
+        public void Display() { 
+        for(int i=0; i<max; i++)
+        {
+
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("FIZZBUZZ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("FIZZ");
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("BUZZ");
+
+                }
+                else {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FizzBuzz fz1 = new FizzBuzz(20);
+            fz1.Display();
+        }
+    }
+}
